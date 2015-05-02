ad1c15e19b87db69b7310730c1f5bc3d61bfcdf0
Aaron Schumacher <ajschumacher@gmail.com>
2015-05-01 21:58:38 -0400

    initial with data setup"

---
+++ data.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00300/Tennis-Major-Tournaments-Match-Statistics.zip
+
+unzip Tennis-Major-Tournaments-Match-Statistics.zip
