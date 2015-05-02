d3adeb6dad12eceb30fee422639037db0940fb86
Aaron Schumacher <ajschumacher@gmail.com>
2015-05-02 12:39:11 -0400

    move getch to other file

---
+++ getch.py
@@ -0,0 +1,25 @@
+def _find_getch():
+    # courtesy of Louis
+    # http://stackoverflow.com/questions/510357/
+    try:
+        import termios
+    except ImportError:
+        # Non-POSIX. Return msvcrt's (Windows') getch.
+        import msvcrt
+        return msvcrt.getch
+
+    # POSIX system. Create and return a getch that manipulates the tty.
+    import sys, tty
+    def _getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    return _getch
+
+getch = _find_getch()
--- uno.py
+++ uno.py
@@ -6,33 +6,6 @@ import re
 import Levenshtein
 
 
-def _find_getch():
-    # courtesy of Louis
-    # http://stackoverflow.com/questions/510357/
-    try:
-        import termios
-    except ImportError:
-        # Non-POSIX. Return msvcrt's (Windows') getch.
-        import msvcrt
-        return msvcrt.getch
-
-    # POSIX system. Create and return a getch that manipulates the tty.
-    import sys, tty
-    def _getch():
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-    return _getch
-
-getch = _find_getch()
-
-
 def initialize(name):
     initial = re.match("^[A-Z]", name).group()
     last = re.search("(?<=[ .])[A-Z].+$", name).group()
