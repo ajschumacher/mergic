b28989e16a4d04bdd59e26be4148e53a8cb20424
Aaron Schumacher <ajschumacher@gmail.com>
2015-05-09 19:17:44 -0400

    ugly but initial grouping produced

--- groups.py
+++ groups.py
@@ -1,17 +1,26 @@
+import json
 import Levenshtein
 from fuzzywuzzy import fuzz
 from itertools import combinations
 from collections import Counter
+from collections import OrderedDict
 from uno import initialize
-
 
 data_file_name = 'names_all.txt'
 # data_file_name = 'RLdata500.csv'
+
+
+def link_items(belongings, links):
+    for one, other in links:
+        if belongings[one] is belongings[other]:
+            continue
+        else:
+            union = belongings[one] + belongings[other]
+            for thing in union:
+                belongings[thing] = union
+
 with open(data_file_name) as f:
     sets = {name.strip(): (name.strip(),) for name in f.readlines()}
-
-# sets = {a: {a} for a in ['aa', 'ab', 'ac', 'ad']}
-
 
 # distance = Levenshtein.distance
 # distance = lambda x, y: 100 - fuzz.ratio(x, y)
@@ -30,16 +39,27 @@ for cutoff in cutoffs:
     # alternative way to grow groups: on a per-group basis
     # rather than globally changing cutoff, could just grow
     # groups until they reach some "satisfactory" size
-    for one, other in links_at[cutoff]:
-        if sets[one] is sets[other]:
-            continue
-        else:
-            union = sets[one] + sets[other]
-            for thing in union:
-                sets[thing] = union
+    link_items(sets, links_at[cutoff])
     unique_sets = []
     for a_set in sets.values():
         if a_set not in unique_sets:
             unique_sets.append(a_set)
     tables[cutoff] = (Counter(len(x) for x in unique_sets),
                       sum(len(x)*(len(x)-1)/2 for x in unique_sets))
+ordered_items = sets.values()[0]
+
+# NOT DRY (copied from above)
+with open(data_file_name) as f:
+    sets = {name.strip(): (name.strip(),) for name in f.readlines()}
+for cutoff in [x for x in cutoffs if x <= 2]:
+    link_items(sets, links_at[cutoff])
+unique_sets = []
+for a_set in sets.values():
+    if a_set not in unique_sets:
+        unique_sets.append(a_set)
+unique_sets.sort(key=lambda x: (0-len(x), ordered_items.index(x[0])))
+result = OrderedDict()
+for item in unique_sets:
+    result[max(item, key=len)] = list(item)
+
+print json.dumps(result, indent=4, separators=(',', ': '))
